d32cbf4e9df3e8f1a70d4e39a483e1179b86a95f
NielsNL4 <34681292+NielsNL4@users.noreply.github.com>
2018-06-04 10:16:10 +0200

    first commit

---
+++ TextAdventure/src/Game.cs
@@ -0,0 +1,143 @@
+﻿using System;
+
+namespace ZuulCS
+{
+	public class Game
+	{
+		private Parser parser;
+        private Player player;
+
+		public Game ()
+		{
+            player = new Player();
+            parser = new Parser();
+
+            player.createRooms();
+		}
+
+		/**
+	     *  Main play routine.  Loops until end of play.
+	     */
+		public void play()
+		{
+			printWelcome();
+
+			// Enter the main command loop.  Here we repeatedly read commands and
+			// execute them until the game is over.
+			bool finished = false;
+			while (! finished) {
+				Command command = parser.getCommand();
+				finished = processCommand(command);
+            }
+			Console.WriteLine("Thank you for playing.");
+
+		}
+
+		/**
+	     * Print out the opening message for the player.
+	     */
+		private void printWelcome()
+		{
+			Console.WriteLine();
+			Console.WriteLine("Welcome to Zuul!");
+			Console.WriteLine("Zuul is a new, incredibly boring adventure game.");
+			Console.WriteLine("Type 'help' if you need help.");
+			Console.WriteLine();
+			Console.WriteLine(player.currentRoom.getLongDescription());
+		}
+
+		/**
+	     * Given a command, process (that is: execute) the command.
+	     * If this command ends the game, true is returned, otherwise false is
+	     * returned.
+	     */
+		private bool processCommand(Command command)
+		{
+			bool wantToQuit = false;
+
+			if(command.isUnknown()) {
+				Console.WriteLine("I don't know what you mean...");
+				return false;
+			}
+
+			string commandWord = command.getCommandWord();
+			switch (commandWord) {
+				case "help":
+					printHelp();
+					break;
+				case "go":
+					goRoom(command);
+					break;
+				case "quit":
+					wantToQuit = true;
+					break;
+                case "look":
+                    goLook();
+                    break;
+                case "health":
+                    showHealth();
+                    break;
+			}
+
+			return wantToQuit;
+		}
+
+		// implementations of user commands:
+
+		/**
+	     * Print out some help information.
+	     * Here we print some stupid, cryptic message and a list of the
+	     * command words.
+	     */
+		private void printHelp()
+		{
+			Console.WriteLine("You are lost. You are alone.");
+			Console.WriteLine("You wander around at the university.");
+			Console.WriteLine();
+			Console.WriteLine("Your command words are:");
+			parser.showCommands();
+		}
+
+        private void goLook()
+        {
+            Console.WriteLine(player.currentRoom.getLongDescription());
+        }
+
+		/**
+	     * Try to go to one direction. If there is an exit, enter the new
+	     * room, otherwise print an error message.
+	     */
+		private void goRoom(Command command)
+		{
+			if(!command.hasSecondWord()) {
+				// if there is no second word, we don't know where to go...
+				Console.WriteLine("Go where?");
+				return;
+            }
+            else
+            {
+
+                player.Damage(5);
+                player.isAlive();
+            }
+
+			string direction = command.getSecondWord();
+
+			// Try to leave current room.
+			Room nextRoom = player.currentRoom.getExit(direction);
+
+			if (nextRoom == null) {
+				Console.WriteLine("There is no door to "+direction+"!");
+			} else {
+				player.currentRoom = nextRoom;
+				Console.WriteLine(player.currentRoom.getLongDescription());
+			}
+		}
+
+        private void showHealth()
+        {
+            Console.WriteLine(player.health);
+        }
+
+	}
+}
---
+++ TextAdventure/src/Player.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace ZuulCS
+{
+    class Player
+    {
+        private Room _currentRoom;
+        public uint health = 100;
+
+        public void createRooms()
+        {
+            Room outside, theatre, pub, lab, office, cloud, uranus, ded;
+
+            // create the rooms
+            outside = new Room("outside the main entrance of the university");
+            theatre = new Room("in a lecture theatre");
+            pub = new Room("in the campus pub");
+            lab = new Room("in The Lab");
+            cloud = new Room("on a cloud");
+            office = new Room("in the office");
+            uranus = new Room("in uranus");
+            ded = new Room("splashed to ur death");
+
+            // initialise room exits
+            outside.setExit("east", theatre);
+            outside.setExit("south", lab);
+            outside.setExit("west", pub);
+            outside.setExit("up", cloud);
+
+            theatre.setExit("west", outside);
+
+            pub.setExit("east", outside);
+
+            lab.setExit("south", office);
+            lab.setExit("north", outside);
+
+            office.setExit("west", lab);
+
+            cloud.setExit("down", outside);
+            cloud.setExit("up", uranus);
+
+            uranus.setExit("down", cloud);
+
+            _currentRoom = outside;  // start game outside
+        }
+
+        public Room currentRoom
+        {
+            get { return this._currentRoom; }
+            set { this._currentRoom = value; }
+        }
+
+        public void isAlive()
+        {
+            if (health > 0)
+            {
+                Console.WriteLine("Health: " + health);
+            }else if(health <= 0)
+            {
+                Console.WriteLine("You Died");
+            }
+        }
+
+        public void Damage(uint amount)
+        {
+            health -= amount;
+        }
+
+        public void heal(uint amount)
+        {
+            health += amount;
+        }
+
+    }
+}
