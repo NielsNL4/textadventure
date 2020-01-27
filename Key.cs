44d4ce26857f6b552c6bb50d6917fb2399471bae
NielsNL4 <34681292+NielsNL4@users.noreply.github.com>
2020-01-27 13:13:07 +0100

    found key.cs

---
+++ Key.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZuulCS;
+
+namespace TextAdventure.src
+{
+    class Key : Item
+    {
+
+        public Key()
+        {
+            name = "key";
+            description = "an old Key";
+        }
+    }
+}
